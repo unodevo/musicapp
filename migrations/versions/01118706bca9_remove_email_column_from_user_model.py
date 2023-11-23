9efbf879f2e297ac2c4e9ca4f9e607b02919815e
stefania <stefaniaannunziata94@gmail.com>
2023-11-23 21:04:46 +0000

    Update Heroku configuration

---
+++ migrations/versions/01118706bca9_remove_email_column_from_user_model.py
@@ -0,0 +1,24 @@
+"""Remove email column from User model
+
+Revision ID: 01118706bca9
+Revises: 5149abd5e1d6
+Create Date: 2023-11-23 20:35:18.592751
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '01118706bca9'
+down_revision = '5149abd5e1d6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/8ef722e637b0_remove_email_column_from_user_model.py
@@ -0,0 +1,24 @@
+"""Remove email column from User model
+
+Revision ID: 8ef722e637b0
+Revises: 01118706bca9
+Create Date: 2023-11-23 20:39:32.443757
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8ef722e637b0'
+down_revision = '01118706bca9'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
